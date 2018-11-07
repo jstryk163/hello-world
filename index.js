52477aa2acf870fe5551b1bd0d38a21faecb3433
jstryk163 <jstryker173@gmail.com>
2018-11-07 10:32:53 -0500

    sg

---
+++ index.js
@@ -0,0 +1,12 @@
+function consolePrint(){
+  console.log("Hello, World!");
+}
+
+function alertPrint(){
+  alert("Hello, World!");
+}
+
+function pprint(){
+  var p = document.getElementById("Hello");
+  p.innerHTML = "Hello, World!";
+}
